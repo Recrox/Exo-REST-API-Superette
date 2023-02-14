925f886a3faab61b9e1a0c0031de0668e94f1cde
Arno Leruste <recroX91@hotmail.com>
2023-02-14 11:00:01 +0100

    Creation de la structure du projet ainsi que l'ajoute d'un startup pour build plus facilement l'application.

---
+++ Exo REST API Superette/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace Technocite.Auchan.Superette.Site
+{
+    public class Startup
+    {
+        /// <summary>
+        /// Gets or sets the Configuration.
+        /// </summary>
+        public IConfiguration Configuration { get; set; }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration parameters.</param>
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
