ceb36d41c1a1d5e617e22a33442c6b114a080b9b
Recrox <Recrox91@hotmail.com>
2023-02-15 12:01:57 +0100

    fusssiioon

---
+++ WebSuperette/Controllers/CategoryArticleController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Technocite.Auchan.Superette.Buisness.Interfaces;
+using Technocite.Auchan.Superette.Site.ViewModels;
+
+namespace Technocite.Auchan.Superette.Site.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoryArticleController : ControllerBase
+    {
+        private readonly ICategoryArticleDomain categoryArticleDomain;
+        private readonly IMapper mapper;
+
+        public CategoryArticleController(ICategoryArticleDomain categoryArticleDomain, IMapper mapper)
+        {
+            this.categoryArticleDomain = categoryArticleDomain;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var CategoryArticle =  this.categoryArticleDomain.GetAll();
+
+            return this.Ok(this.mapper.Map<IEnumerable<CategoryArticle>>(CategoryArticle));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(CategoryArticle categoryArticle) 
+        {
+            await this.categoryArticleDomain.AddAsync(this.mapper.Map<Core.Models.CategoryArticle>(categoryArticle));
+            return this.Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteByIdAsync(int id)
+        {
+            try
+            {
+                await this.categoryArticleDomain.RemoveByIdAsync(id);
+                return this.Ok();
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAsync(CategoryArticle categoryArticle) 
+        {
+            try
+            {
+                await this.categoryArticleDomain.UpdateAsync(this.mapper.Map<Core.Models.CategoryArticle>(categoryArticle));
+                return this.Ok();
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(e.Message);
+            }
+        }
+    }
+}
