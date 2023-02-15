7915c4ffb90b005e1b5dde9074b07465e3abb86b
Recrox <Recrox91@hotmail.com>
2023-02-15 10:26:13 +0100

    adding consigne de l'exo

--- Providers/Models/Ticket.cs
+++ Providers/Models/Ticket.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Providers.Models
+﻿namespace Providers.Models
 {
-    internal class Ticket
+    public class Ticket
     {
+        public int Id { get; set; }
     }
 }
